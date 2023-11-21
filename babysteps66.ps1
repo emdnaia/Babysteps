d88ad4fdb3e1842b2dbe78557cca9699a93aea95
emdnaia <148504452+emdnaia@users.noreply.github.com>
2023-11-21 09:51:21 +0100

    Update babysteps66.ps1

--- babysteps66.ps1
+++ babysteps66.ps1
@@ -1,5 +1,4 @@
-# Original upload: https://pastebin.com/Qhrbmy8R
-# v6.6.1 - with key splitting
+# v6.6.2 - with key splitting and string reversal
 
 # Prompt for user input with split strings
 $promptPart1 = 'Enter '
@@ -18,6 +17,9 @@ $aliasConvertToBase64 = 'ToBase64String'
 $encryptedBytes = $encryptor.TransformFinalBlock([Text.Encoding]::UTF8.GetBytes($cmd), 0, $cmd.Length)
 $encryptedCmd = [Convert]::$aliasConvertToBase64($encryptedBytes)
 
+# Reverse the encrypted command
+$reversedEncryptedCmd = -join ($encryptedCmd[-1..-($encryptedCmd.Length)])
+
 # Split the key into multiple parts with random case
 $Key = [Convert]::ToBase64String($encryptionObject.Key)
 $keyPart1 = $Key.Substring(0, 8)
@@ -27,10 +29,11 @@ $keyPart3 = $Key.Substring(16)
 # Store IV as Base64 with dynamic variable names
 $Base64IVVarName = -join ('IV' | Get-Random -Count 10)
 $Base64IV = [Convert]::ToBase64String($encryptionObject.IV)
- 
-# Decryption command with adjusted key and IV handling, including key splitting
-$decryptFuncName = -join ('DecryptCmd' | Get-Random -Count 10)
-$decryptCmd = "function $decryptFuncName{param(`$eCmd, `$k1, `$k2, `$k3, `$iv)`$AES=New-Object Security.Cryptography.AesManaged;`$keyBytesList=New-Object System.Collections.ArrayList;`$keyBytesList.AddRange([Convert]::FromBase64String(`$k1));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k2));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k3));`$keyBytes=`$keyBytesList.ToArray();`$AES.IV=[Convert]::FromBase64String(`$iv);`$decryptor=`$AES.CreateDecryptor(`$keyBytes, `$AES.IV);`$eBytes=[Convert]::FromBase64String(`$eCmd);`$dBytes=`$decryptor.TransformFinalBlock(`$eBytes,0,`$eBytes.Length);return [Text.Encoding]::UTF8.GetString(`$dBytes)}; iex ($decryptFuncName '$encryptedCmd' '$keyPart1' '$keyPart2' '$keyPart3' '$Base64IV')"
 
-Write-Host "To execute your command, copy and paste the following line:" 
+# Constructing a single-line decryption command
+$decryptFuncName = -join ('DecryptCmd' | Get-Random -Count 10)
+$decryptCmd = "function $decryptFuncName{param(`$eCmd,`$k1,`$k2,`$k3,`$iv)`$AES=New-Object Security.Cryptography.AesManaged;`$keyBytesList=New-Object System.Collections.ArrayList;`$keyBytesList.AddRange([Convert]::FromBase64String(`$k1));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k2));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k3));`$keyBytes=`$keyBytesList.ToArray();`$AES.IV=[Convert]::FromBase64String(`$iv);`$decryptor=`$AES.CreateDecryptor(`$keyBytes,`$AES.IV);`$reversedEBytes=-join(`$eCmd[-1..-(`$eCmd.Length)]);`$eBytes=[Convert]::FromBase64String(`$reversedEBytes);`$dBytes=`$decryptor.TransformFinalBlock(`$eBytes,0,`$eBytes.Length);return [Text.Encoding]::UTF8.GetString(`$dBytes)};iex ($decryptFuncName '$reversedEncryptedCmd' '$keyPart1' '$keyPart2' '$keyPart3' '$Base64IV')"
+
+# Output the command as a single line
+Write-Host "To execute your command, copy and paste the following line:"
 Write-Host $decryptCmd
