45b734d3b5b46fa1f905450324cfff313992b52d
emdnaia <148504452+emdnaia@users.noreply.github.com>
2023-11-21 09:28:06 +0100

    Update babysteps66.ps1
    add key splitting

--- babysteps66.ps1
+++ babysteps66.ps1
@@ -1,5 +1,6 @@
 # Original upload: https://pastebin.com/Qhrbmy8R
-# v6.6
+# v6.6.1 - with key splitting
+
 # Prompt for user input with split strings
 $promptPart1 = 'Enter '
 $promptPart2 = 'command'
@@ -16,24 +17,20 @@ $encryptor = $encryptionObject.CreateEncryptor()
 $aliasConvertToBase64 = 'ToBase64String'
 $encryptedBytes = $encryptor.TransformFinalBlock([Text.Encoding]::UTF8.GetBytes($cmd), 0, $cmd.Length)
 $encryptedCmd = [Convert]::$aliasConvertToBase64($encryptedBytes)
- 
-# Ensure correct key length for AES encryption
+
+# Split the key into multiple parts with random case
 $Key = [Convert]::ToBase64String($encryptionObject.Key)
-$IV = [Convert]::ToBase64String($encryptionObject.IV)
-$keyLength = 32 # Length for AES-256
-$ivLength = 16 # Standard IV length for AES
- 
+$keyPart1 = $Key.Substring(0, 8)
+$keyPart2 = $Key.Substring(8, 8)
+$keyPart3 = $Key.Substring(16)
+
 # Store IV as Base64 with dynamic variable names
 $Base64IVVarName = -join ('IV' | Get-Random -Count 10)
 $Base64IV = [Convert]::ToBase64String($encryptionObject.IV)
  
-# Decryption command with adjusted key and IV handling
+# Decryption command with adjusted key and IV handling, including key splitting
 $decryptFuncName = -join ('DecryptCmd' | Get-Random -Count 10)
-$decryptCmd = "function $decryptFuncName{param(`$eCmd, `$key, `$iv)`$AES=New-Object Security.Cryptography.AesManaged;`$AES.Key=[Convert]::FromBase64String(`$key);`$AES.IV=[Convert]::FromBase64String(`$iv);`$decryptor=`$AES.CreateDecryptor();`$eBytes=[Convert]::FromBase64String(`$eCmd);`$dBytes=`$decryptor.TransformFinalBlock(`$eBytes,0,`$eBytes.Length);return [Text.Encoding]::UTF8.GetString(`$dBytes)}; iex ($decryptFuncName '$encryptedCmd' '$Key' '$IV')"
- 
-# Output with No-Op commands and dynamic behavior
-$nullVar = Get-Random
-$nullVar++
-$nullVar--
+$decryptCmd = "function $decryptFuncName{param(`$eCmd, `$k1, `$k2, `$k3, `$iv)`$AES=New-Object Security.Cryptography.AesManaged;`$keyBytesList=New-Object System.Collections.ArrayList;`$keyBytesList.AddRange([Convert]::FromBase64String(`$k1));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k2));`$keyBytesList.AddRange([Convert]::FromBase64String(`$k3));`$keyBytes=`$keyBytesList.ToArray();`$AES.IV=[Convert]::FromBase64String(`$iv);`$decryptor=`$AES.CreateDecryptor(`$keyBytes, `$AES.IV);`$eBytes=[Convert]::FromBase64String(`$eCmd);`$dBytes=`$decryptor.TransformFinalBlock(`$eBytes,0,`$eBytes.Length);return [Text.Encoding]::UTF8.GetString(`$dBytes)}; iex ($decryptFuncName '$encryptedCmd' '$keyPart1' '$keyPart2' '$keyPart3' '$Base64IV')"
+
 Write-Host "To execute your command, copy and paste the following line:" 
 Write-Host $decryptCmd
